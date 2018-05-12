25a1f9c0282894f012c5df7f32acd8f30f5ddf8d
Nur Ain Basyirah <ainbasyirah15@gmail.com>
2018-05-12 16:35:55 +0800

    Create profdriver1.xaml.cs

---
+++ profdriver1.xaml.cs
@@ -0,0 +1,60 @@
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WPF_Bustrack
+{
+    /// <summary>
+    /// Interaction logic for driverprof2.xaml
+    /// </summary>
+    public partial class profdriver1 : Window
+    {
+        public profdriver1()
+        {
+            InitializeComponent();
+            binddatagrid();
+        }
+
+        private void binddatagrid()
+        {
+            SqlConnection con = new SqlConnection();
+            con.ConnectionString = ConfigurationManager.ConnectionStrings["connprofile"].ConnectionString;
+            con.Open();
+            SqlCommand cmd = new SqlCommand();
+            cmd.CommandText = "select * from [tblprofile] ";
+            cmd.Connection = con;
+            SqlDataAdapter sda = new SqlDataAdapter(cmd);
+            DataTable dt = new DataTable("tblprofile");
+            sda.Fill(dt);
+
+            g1.ItemsSource = dt.DefaultView;
+        }
+
+        private void btngps_Click(object sender, RoutedEventArgs e)
+        {
+            gpsmap scgpsmap = new gpsmap();
+            scgpsmap.Show();
+            this.Close();
+        }
+
+        private void btncamera_Click(object sender, RoutedEventArgs e)
+        {
+            camera sccamera = new camera();
+            sccamera.Show();
+            this.Close();
+        }
+    }
+}
